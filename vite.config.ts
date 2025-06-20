9854b014feb8508d5bc98ec5947f981b6aea47e5
Pedro <92474347+Pedroffda@users.noreply.github.com>
2025-06-20 12:32:17 -0300

    fix: remove base URL from Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/pyne2025-vending-machine/", 
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
