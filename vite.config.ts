903279481c5f67be6ce64bd6ca97be7cc52fb82c
Pedro <92474347+Pedroffda@users.noreply.github.com>
2025-06-20 12:08:16 -0300

    fix: update base URL in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "https://pedroffda.github.io/pyne2025-vending-machine/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
