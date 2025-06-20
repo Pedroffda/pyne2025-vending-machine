a02e931a7c0a77aad78ec9ff8dda0e9b2065f586
Pedro <92474347+Pedroffda@users.noreply.github.com>
2025-06-20 12:10:41 -0300

    fix: update base URL in Vite configuration to relative path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://pedroffda.github.io/pyne2025-vending-machine/",
+  base: "/pyne2025-vending-machine/", 
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
